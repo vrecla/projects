ad0aad45cf0b9a4ee2d315a40a77c93da5434274
Vanessa Recla <66720063+vrecla@users.noreply.github.com>
2024-07-13 17:11:34 +0800

    Update description

--- expo.py
+++ expo.py
@@ -1,3 +1,5 @@
+#This project aims to scrape all companies and its other details in a conference website using Selenium.
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
